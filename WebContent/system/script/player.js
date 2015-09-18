f3cc621a3dd208fe36290d61b234b60f157460fe
Braulio Ribeiro <braulio_holtz@hotmail.com>
2015-09-18 12:03:23 -0300

    Revert "Merge branch 'master' of https://github.com/mrsplendid/PLAYz-Media-for-PLEX"
    This reverts commit 93f2f7be0889e95aab598fa697f728d71bd61241, reversing
    changes made to 3d9d7756c1a45bceaac6e36d5656f63dfc0dffb2.

--- WebContent/system/script/player.js
+++ WebContent/system/script/player.js
@@ -44,7 +44,7 @@ Player.prototype.initialise = function()
 	this.plex = new PLEX();
 	this.key = $.querystring().key;
 	this.media = document.getElementById("player");		
-		
+	this.mediaSource = document.getElementById("mediaSource");		
 
 	//Direct play via standalone player
 	if (self.directPlay) {
@@ -152,7 +152,14 @@ Player.prototype.initialise = function()
 				$("#progressTime").text("");
 	});
 	
-
+	// seek() after load()
+	this.media.addEventListener('canplay', function() {
+		this.hideLoader();
+		if (this.seekNewTime) {
+			this.seek(this.seekNewTime);
+			this.seekNewTime = 0;
+		}
+	}.bind(this));
 	
 	// Loader
 	this.media.addEventListener('seeking', function() {
@@ -409,7 +416,8 @@ Player.prototype.openMedia = function(key)
 			}			
 			
 			//Integrated player
-			
+			self.mediaSource.src = self.plex.getServerUrl() + self.mediaUrl;
+			self.media.load();
 			if ($(xml).find("Stream[streamType='2']").length <= 1) {
 				$("#language").hide();		
 			} else {
